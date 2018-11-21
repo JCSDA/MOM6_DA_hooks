0b710e7a0f4687628389ea873f799036f745f7f2
Rahul Mahajan <aerorahul@users.noreply.github.com>
2018-11-21 12:46:30 -0500

    update Intel Fortran compiler flags to be consistent with MOM6

--- cmake/compiler_flags_Intel_Fortran.cmake
+++ cmake/compiler_flags_Intel_Fortran.cmake
@@ -13,17 +13,19 @@ else( )
   set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qopenmp-stubs")
 endif( )
 
+set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ftz -align all -fno-alias -traceback -r8 -stack_temps -safe_cray_ptr -assume byterecl -fp-model source -convert big_endian -fPIC -fpe0 -heap-arrays 32 -assume noold_maxminloc -align dcommons")
+
 ####################################################################
 # RELEASE FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_RELEASE "-O3 -qopt-report0 -ftz -align all -fno-alias -qno-offload -traceback -r8   -stack_temps -safe_cray_ptr -assume byterecl -fp-model source -ftz -align all -fno-alias -align dcommons -convert big_endian  -fPIC -fpe0 -fp-model source -heap-arrays 32 -assume noold_maxminloc  -align dcommons" )
+set( CMAKE_Fortran_FLAGS_RELEASE "-O3 -qopt-report0 -qno-offload" )
 
 ####################################################################
 # DEBUG FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_DEBUG   "-g -O0 -ftz -align all -fno-alias -traceback -debug -nolib-inline -fno-inline-functions -assume protect_parens,minus0 -prec-div -prec-sqrt -check bounds -check uninit -fp-stack-check -ftrapuv -warn unused -traceback -r8 -stack_temps -safe_cray_ptr -assume byterecl -fp-model source -ftz -align all -fno-alias -align dcommons -convert big_endian  -fPIC -fpe0 -fp-model source -heap-arrays 32 -assume noold_maxminloc  -align dcommons" )
+set( CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g -check bounds -warn -heap-arrays -fpe-all=0 -fpe:0 -check assume -check format -check output_conversion -check pointers -check stack -check uninit" )
 
 ####################################################################
 # BIT REPRODUCIBLE FLAGS
